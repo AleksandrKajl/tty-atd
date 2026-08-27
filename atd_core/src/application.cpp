57bc605c50a0284cf58139a9235bfdec437f6ed4
a.kajl <kajlaleksandr1986@gmail.com>
2026-08-27 07:06:42 +0300

    dev: реализован фасад сервера

--- atd_core/src/application.cpp
+++ atd_core/src/application.cpp
@@ -1,1 +1,15 @@
 #include "atd/application.hpp"
+#include "atd/command_dictionary.hpp"
+#include "atd/serial_port.hpp"
+#include "atd/server.hpp"
+
+namespace atd {
+
+void runServer(const ServerConfig& config) {
+    CommandDictionary dictionary(config.dictionaryPath);
+    SerialPort port(config.devicePath, config.baudRate);
+
+    AtServer server(port, dictionary);
+    server.run();
+}
+} // namespace atd
